253324ecf2b285167ac4c5b7afc92f438ed35cf4
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-10-19 20:37:29 -0300

    add(routes): To get profile, messages

--- node/src/infra/http/routes/index.ts
+++ node/src/infra/http/routes/index.ts
@@ -2,6 +2,8 @@ import {Router} from "express";
 import {authenticateUserController} from "../../../modules/githubSocialAuthentication/useCases/authenticateUser";
 import {createMessageController} from "../../../modules/sendMessages/useCases/createMessages";
 import {ensureAuthenticated} from "../middleware/ensureAuthenticated";
+import {getLast3MessageController} from "../../../modules/sendMessages/useCases/getLast3Messages";
+import {profileUserController} from "../../../modules/githubSocialAuthentication/useCases/profileUser";
 
 const routes = Router();
 
@@ -11,6 +13,14 @@ routes.post("/authenticate/github", (request, response) => {
 
 routes.post("/messages", ensureAuthenticated, (request, response) => {
     createMessageController.handle(request, response);
+});
+
+routes.get("/messages", (request, response) => {
+    getLast3MessageController.handle(request, response);
+});
+
+routes.get("/profile", ensureAuthenticated, (request, response) => {
+    profileUserController.handle(request, response);
 });
 
 export {routes};

ddb84cac2d1c25f9a28717aa04ee5c18299c0283
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-10-19 00:39:53 -0300

    created(infra): Messages routes and auth middleware

--- node/src/infra/http/routes/index.ts
+++ node/src/infra/http/routes/index.ts
@@ -1,8 +1,16 @@
 import {Router} from "express";
-import {authRoutes} from "./auth.routes";
+import {authenticateUserController} from "../../../modules/githubSocialAuthentication/useCases/authenticateUser";
+import {createMessageController} from "../../../modules/sendMessages/useCases/createMessages";
+import {ensureAuthenticated} from "../middleware/ensureAuthenticated";
 
 const routes = Router();
 
-routes.use("/authenticate", authRoutes);
+routes.post("/authenticate/github", (request, response) => {
+    authenticateUserController.handle(request, response);
+});
+
+routes.post("/messages", ensureAuthenticated, (request, response) => {
+    createMessageController.handle(request, response);
+});
 
 export {routes};
